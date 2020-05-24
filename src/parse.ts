c1b9852aa948a94e7175598ff9471f50b94bef0a
Marat <mamirov@ubic.tech>
2020-05-24 15:25:06 +0300

    add type and format validation

--- src/parse.ts
+++ src/parse.ts
@@ -1,4 +1,8 @@
-import { PropertyRequiredError } from './errors/validation';
+import {
+  PropertyRequiredError,
+  PropertyTypeError,
+  PropertyFormatError,
+} from './errors/validation';
 
 type Options = {
   [option: string]: number | string,
@@ -9,7 +13,7 @@ type Options = {
   endDate: string,
 }
 
-function validate(data: Options): void {
+function validateProperty(data: Options): void {
   [
     'amount',
     'interest',
@@ -23,8 +27,37 @@ function validate(data: Options): void {
     });
 }
 
+function validatePropertyType(data: Options): void {
+  const regexDate = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/g;
+  [
+    'amount',
+    'term',
+  ]
+    .forEach((option) => {
+      if (Number.isNaN(parseInt(data[option] as string, 10))) {
+        throw new PropertyTypeError(option);
+      }
+    });
+
+  if (typeof data.interest === 'string'
+    && Number.isNaN(parseFloat(data.interest))) {
+    throw new PropertyTypeError('interest');
+  }
+
+  [
+    'startDate',
+    'endDate',
+  ]
+    .forEach((option) => {
+      if (!data[option].toString().match(regexDate)) {
+        throw new PropertyFormatError(option);
+      }
+    });
+}
+
 export default (data: string) => {
   const parsed = JSON.parse(data);
-  validate(parsed);
+  validateProperty(parsed);
+  validatePropertyType(parsed);
   return parsed;
 };
