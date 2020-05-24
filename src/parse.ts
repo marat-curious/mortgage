c75286f6dae0ee23710937fb0e63ecfd142d4c28
Marat <mamirov@ubic.tech>
2020-05-24 13:53:21 +0300

    add validation function

--- src/parse.ts
+++ src/parse.ts
@@ -1,1 +1,30 @@
-export default () => [];
+import { PropertyRequiredError } from './errors/validation';
+
+type Options = {
+  [option: string]: number | string,
+  amount: number,
+  interest: number,
+  term: number,
+  startDate: string,
+  endDate: string,
+}
+
+function validate(data: Options): void {
+  [
+    'amount',
+    'interest',
+    'term',
+    'startDate',
+    'endDate',
+  ]
+    .filter((option) => typeof data[option] === 'undefined')
+    .forEach((option) => {
+      throw new PropertyRequiredError(option);
+    });
+}
+
+export default (data: string) => {
+  const parsed = JSON.parse(data);
+  validate(parsed);
+  return parsed;
+};
